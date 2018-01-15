e2ea2bc195031bf48a45f2a57951bf51dc76d4f4
Nazar Sekh <Nazar.sekh@gmail.com>
2018-01-15 19:53:13 +0300

    add mapper

---
+++ persistence/src/main/java/com/mpls/v2/dto/utils/annotations/Dto.java
@@ -0,0 +1,10 @@
+package com.mpls.v2.dto.utils.annotations;
+
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Dto {
+
+}
