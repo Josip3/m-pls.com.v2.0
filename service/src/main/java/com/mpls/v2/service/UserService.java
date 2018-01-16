6c2186210d3a5c9d95039f3a6372a45e9215e5b4
Nazar Sekh <Nazar.sekh@gmail.com>
2018-01-16 15:39:10 +0300

    i down

--- service/src/main/java/com/mpls/v2/service/UserService.java
+++ service/src/main/java/com/mpls/v2/service/UserService.java
@@ -4,18 +4,15 @@ import com.mpls.v2.model.User;
 
 import java.util.List;
 
-/**
- * Created by danul on 18.10.2017.
- */
 public interface UserService {
 
-    User save(User user);//post
+    User save(User user);
 
-    List<User> findAll();//get
+    List<User> findAll();
 
-    User findOne(Long id);//get
+    User findOne(Long id);
 
-    Boolean delete(Long id);//delete
+    Boolean delete(Long id);
 
     List<User> findByFirstName(String firstName);
 
