0039d12d6ec6de1088ac39b29eda0de37d3063a1
Illya Chenchak <chen0899@M-PLUS.local>
2018-01-16 16:33:00 +0200

    updateBlog

--- service/src/main/java/com/mpls/v2/service/BlogService.java
+++ service/src/main/java/com/mpls/v2/service/BlogService.java
@@ -1,5 +1,6 @@
 package com.mpls.v2.service;
 
+import com.mpls.v2.dto.BlogDto;
 import com.mpls.v2.model.Blog;
 
 import java.util.List;
@@ -7,6 +8,8 @@ import java.util.List;
 public interface BlogService {
 
     Blog save(Blog blog);
+
+    Blog update(BlogDto blogDto);
 
     List<Blog> findAll();
 
--- service/src/main/java/com/mpls/v2/service/impl/BlogServiceImpl.java
+++ service/src/main/java/com/mpls/v2/service/impl/BlogServiceImpl.java
@@ -1,16 +1,20 @@
 package com.mpls.v2.service.impl;
 
+import com.mpls.v2.dto.BlogDto;
 import com.mpls.v2.model.Blog;
 import com.mpls.v2.repository.BlogRepository;
 import com.mpls.v2.service.BlogService;
 import com.mpls.v2.service.exceptions.FindException;
 import com.mpls.v2.service.exceptions.IdException;
 import com.mpls.v2.service.exceptions.SaveException;
+import com.mpls.v2.service.exceptions.UpdateException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 import java.util.List;
+
+import static com.mpls.v2.dto.utils.builder.Builder.map;
 
 @Service
 public class BlogServiceImpl implements BlogService {
@@ -26,6 +30,16 @@ public class BlogServiceImpl implements BlogService {
             throw new SaveException("blog muast be not null");
         }
 
+    }
+
+    @Override
+    public Blog update(BlogDto blogDto) {
+        try{
+            Blog blog = blogRepository.findOne(blogDto.getId());
+            return blogRepository.save(map(blogDto,Blog.class).setIndustries(blog.getIndustries()));
+        }catch (Exception e) {
+            throw new UpdateException("cant update");
+        }
     }
 
     @Override
